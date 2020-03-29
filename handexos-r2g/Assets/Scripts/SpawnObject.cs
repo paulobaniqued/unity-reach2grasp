e7438e2a54988fa3bc833e2de9042236a694862e
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2020-03-29 23:06:27 +0100

    Instantiate Prefabs

---
+++ handexos-r2g/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public Rigidbody trialObject;
+    public Transform spawnpoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Debug.Log("1 pressed");
+
+            Rigidbody clone;
+
+            clone = Instantiate(trialObject, spawnpoint.position, spawnpoint.rotation)as Rigidbody;
+        }
+        
+    }
+}
