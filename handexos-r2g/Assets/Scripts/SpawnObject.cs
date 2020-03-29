d6f51ae7c110ffa62953731bfc464fd5c86c7507
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2020-03-29 23:14:04 +0100

    more objects

--- handexos-r2g/Assets/Scripts/SpawnObject.cs
+++ handexos-r2g/Assets/Scripts/SpawnObject.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SpawnObject : MonoBehaviour
 {
-    public Rigidbody trialObject;
+    public Rigidbody trialObject1;
+    public Rigidbody trialObject2;
+    public Rigidbody trialObject3;
     public Transform spawnpoint;
 
     // Update is called once per frame
@@ -14,9 +16,23 @@ public class SpawnObject : MonoBehaviour
         {
             Debug.Log("1 pressed");
 
-            Rigidbody clone;
+            Rigidbody clone1;
+            clone1 = Instantiate(trialObject1, spawnpoint.position, spawnpoint.rotation)as Rigidbody;
+        }
 
-            clone = Instantiate(trialObject, spawnpoint.position, spawnpoint.rotation)as Rigidbody;
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Debug.Log("2 pressed");
+
+            Rigidbody clone2;
+            clone2 = Instantiate(trialObject2, spawnpoint.position, spawnpoint.rotation)as Rigidbody;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            Debug.Log("3 pressed");
+
+            Rigidbody clone3;
+            clone3 = Instantiate(trialObject3, spawnpoint.position, spawnpoint.rotation)as Rigidbody;
         }
         
     }
